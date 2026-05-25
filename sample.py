279fc2fa7f54517245a0156c4971e11f09706f34
Manjuparkavi <manjuparkavi.be.ece@gmail.com>
2026-05-25 22:58:15 +0530

    Updated the changes

--- sample.py
+++ sample.py
@@ -8,3 +8,14 @@ x = int(input("Enter x value: "))
 result = (a * x**2) + (b * x) + c
 
 print("Polynomial Result =", result)
+
+# Polynomial Equation: ax^2 + bx + c
+
+a = int(input("Enter a value: "))
+b = int(input("Enter b value: "))
+c = int(input("Enter c value: "))
+e = int(input("Enter x value: "))
+
+result = (a * e**2) + (b * e) + c
+
+print("Polynomial Result =", result)
