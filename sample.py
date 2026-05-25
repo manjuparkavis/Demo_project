1e92f7c04fa0eb3a9cf3af82e5645c08a5f12d8f
Manjuparkavi <manjuparkavi.be.ece@gmail.com>
2026-05-25 22:39:12 +0530

    Create sample file

---
+++ sample.py
@@ -0,0 +1,10 @@
+# Polynomial Equation: ax^2 + bx + c
+
+a = int(input("Enter a value: "))
+b = int(input("Enter b value: "))
+c = int(input("Enter c value: "))
+x = int(input("Enter x value: "))
+
+result = (a * x**2) + (b * x) + c
+
+print("Polynomial Result =", result)
